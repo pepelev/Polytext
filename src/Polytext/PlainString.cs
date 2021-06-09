eaf43925ac34a238234f3c3e176bdef30de1ecfa
pepelev <kran440@gmail.com>
2021-06-09 23:41:43 +0500

    Introduces PlainString

---
+++ src/Polytext/PlainString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Poly
+{
+    public sealed class PlainString : String
+    {
+        private readonly IReadOnlyList<CodePoint> points;
+
+        public PlainString(IReadOnlyList<CodePoint> points)
+        {
+            this.points = points ?? throw new ArgumentNullException(nameof(points));
+        }
+
+        public override IEnumerator<CodePoint> GetEnumerator() => points.GetEnumerator();
+        public override int Count => points.Count;
+        public override CodePoint this[int index] => points[index];
+    }
+}
